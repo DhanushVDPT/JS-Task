98fd967996133aa2c4850335840ad6b268398328
DhanushVDPT <be19b034@smail.iitm.ac.in>
2021-08-23 11:15:39 +0530

    roller function works

--- Dice Game/app.js
+++ Dice Game/app.js
@@ -1,50 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    function roll() {
-        diceNums = [1, 2, 3, 4, 5 , 6];
-        var num = diceNums[Math.floor(Math.random()*diceNums.length)];
-        return num;
-    }
+//dice roll
+function roll() {
+	setTimeout(function () {
+		var rollp1 = Math.floor(Math.random() * 6) + 1;
+		var rollp2 = Math.floor(Math.random() * 6) + 1;
 
-    let scores = [];
-    let p1clicks = 0;
-    let p2clicks = 0;
-
-    function roll1() {
-        p1clicks += 1;
-        console.log(p1clicks);
-        var p1 = document.getElementById("p1score");
-        var p1Roll = roll();
-        p1.innerHTML = p1Roll;
-        scores[0] = p1Roll;
-    }
-
-    function roll2() {
-        p2clicks += 1;
-        console.log(p2clicks);
-        var p2 = document.getElementById("p2score");
-        var p2Roll = roll();
-        p2.innerHTML = p2Roll;
-        scores[1] = p2Roll;
-    }
-
-    var p1btn = document.getElementById('p1roll-btn');
-    var p2btn = document.getElementById('p2roll-btn');
-    p1btn.addEventListener('click', roll1);
-    p2btn.addEventListener('click', roll2);
-
-    var game = document.getElementsByClassName('game-grid');
-    game.addEventListener('click', () => {
-        if(p1clicks==1 && p2clicks==1){
-            console.log(scores);
-            if(scores[0] > scores[1]) {
-                document.getElementById('winner').innerHTML = 'Player 1 Wins!';
-            }
-            else if (scores[1] == scores[0]) {
-                document.getElementById('winner').innerHTML = 'Draw';
-            }
-            else{
-                document.getElementById('winner').innerHTML = 'Player 2 Wins!';
-            }
-        }
-    })
-})
+		if (rollp1 === rollp2) {
+			document.querySelector("#winner").innerHTML = "Draw!";
+		}
+		else if (rollp1 < rollp2) {
+			document.querySelector("#winner").innerHTML = "Player 2 WINS!";
+		}
+		else {
+			document.querySelector("#winner").innerHTML = "Player 1 WINS!";
+		}
+        document.querySelector("#p1score").innerHTML = rollp1;
+        document.querySelector("#p2score").innerHTML = rollp2;
+	}, 1000);
+}
