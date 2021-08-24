7e98e5c86015db35074496b67d4f8a9c5777d1e1
DhanushVDPT <be19b034@smail.iitm.ac.in>
2021-08-24 10:45:30 +0530

    working simulation

--- Dice Game/app.js
+++ Dice Game/app.js
@@ -1,8 +1,19 @@
+var images = ["./images/dice-01.svg", "./images/dice-02.svg", "./images/dice-03.svg", "./images/dice-04.svg", "./images/dice-05.svg", "./images/dice-06.svg"];
+var dice = document.querySelectorAll("img");
+
 //dice roll
 function roll() {
+	dice.forEach(function(die){
+        die.classList.add("shake");
+    });
 	setTimeout(function () {
+		dice.forEach(function(die){
+            die.classList.remove("shake");
+        });
 		var rollp1 = Math.floor(Math.random() * 6) + 1;
 		var rollp2 = Math.floor(Math.random() * 6) + 1;
+		document.querySelector("#die-1").setAttribute("src", images[rollp1-1]);
+        document.querySelector("#die-2").setAttribute("src", images[rollp2-1]);
 
 		if (rollp1 === rollp2) {
 			document.querySelector("#winner").innerHTML = "DRAW!";
